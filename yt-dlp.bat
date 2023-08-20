19cf88fc0e35f8c919a27601277e26a72b5493eb
zurica <61469060+zurica2013@users.noreply.github.com>
2023-08-20 11:58:31 +0800

    Add cookies option

--- yt-dlp.bat
+++ yt-dlp.bat
@@ -2,11 +2,11 @@
 cd C:\%HOMEPATH%\Downloads\youtube-dl
 set /P url=[45m输入下载地址：[0m
 echo [43m查看可用的视频格式. . .[0m
-yt-dlp -F %url%
+yt-dlp -F %url% --cookies www.bilibili.com_cookies.txt
 
 :format
 set /P fmt=[45m设置下载格式（ID值，组合使用+）：[0m
-yt-dlp -s -f %fmt% %url% >nul 2>info.log
+yt-dlp -s -f %fmt% %url% >nul 2>info.log --cookies www.bilibili.com_cookies.txt
 findstr /C:"Requested format is not available" info.log
 if %errorlevel%==0 (
    echo [41m下载格式不可用！[0m
@@ -27,7 +27,7 @@ set /P sub=[45m设置字幕格式（第3项）：[0m
 
 :download1
 echo [43m下载开始. . .[0m
-yt-dlp -o C:\%HOMEPATH%\Downloads\%%(title)s.%%(ext)s --downloader aria2c --downloader-args aria2c:"-x 16 -k 1M" -f %fmt% --embed-subs --sub-langs %lang% --sub-format %sub% %url%
+yt-dlp -o C:\%HOMEPATH%\Downloads\%%(title)s.%%(ext)s --downloader aria2c --downloader-args aria2c:"-x 16 -k 1M" -f %fmt% --embed-subs --sub-langs %lang% --sub-format %sub% %url% --cookies www.bilibili.com_cookies.txt
 if errorlevel 1 (
    echo [41m下载失败，重新尝试. . .[0m
    goto download1
@@ -37,7 +37,7 @@ if errorlevel 1 (
 
 :download2
 echo [43m下载开始. . .[0m
-yt-dlp -o C:\%HOMEPATH%\Downloads\%%(title)s.%%(ext)s --downloader aria2c --downloader-args aria2c:"-x 16 -k 1M" -f %fmt% %url%
+yt-dlp -o C:\%HOMEPATH%\Downloads\%%(title)s.%%(ext)s --downloader aria2c --downloader-args aria2c:"-x 16 -k 1M" -f %fmt% %url% --cookies www.bilibili.com_cookies.txt
 if errorlevel 1 (
    echo [41m下载失败，重新尝试. . .[0m
    goto download2
